5b02a19dae20b14ee84a2e18f3b12c22e7eadf5d
firyalshafa <161413463+firyalshafa@users.noreply.github.com>
2026-03-13 14:11:21 +0900

    create KtpDto for handling data transfer between layers

--- src/main/java/com/example/tugas4/dto/KtpDto.java
+++ src/main/java/com/example/tugas4/dto/KtpDto.java
@@ -1,7 +1,13 @@
 package com.example.tugas4.dto;
 
 
+import lombok.Data;
 
+@Data
 public class KtpDto {
-
+    private String nomorKtp;
+    private String namaLengkap;
+    private String alamat;
+    private String tanggalLahir;
+    private String jenisKelamin;
 }
