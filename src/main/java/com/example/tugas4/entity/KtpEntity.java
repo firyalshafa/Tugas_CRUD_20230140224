8aa5d80da5d0e6ebb61bac8c95d27039f855f71d
firyalshafa <161413463+firyalshafa@users.noreply.github.com>
2026-03-13 14:11:57 +0900

    create KtpEntity with JPA annotations and database constraints

--- src/main/java/com/example/tugas4/entity/KtpEntity.java
+++ src/main/java/com/example/tugas4/entity/KtpEntity.java
@@ -1,6 +1,22 @@
 package com.example.tugas4.entity;
 
 
-public class KtpEntity {
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDate;
 
+@Entity
+@Table(name = "ktp")
+@Data
+public class KtpEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true, nullable = false)
+    private String nomorKtp;
+    private String namaLengkap;
+    private String alamat;
+    private LocalDate tanggalLahir;
+    private String jenisKelamin;
 }
