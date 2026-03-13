cc235482f1d761a544b74b7ea513d0ab8fc4bc7b
firyalshafa <161413463+firyalshafa@users.noreply.github.com>
2026-03-13 14:09:50 +0900

    define KtpService interface methods for CRUD operations

--- src/main/java/com/example/tugas4/service/KtpService.java
+++ src/main/java/com/example/tugas4/service/KtpService.java
@@ -2,7 +2,14 @@ package com.example.tugas4.service;
 
 
 
+import com.example.tugas4.dto.KtpDto;
+import com.example.tugas4.entity.KtpEntity;
+import java.util.List;
 
 public interface KtpService {
-
+    KtpEntity create(KtpDto dto);
+    List<KtpEntity> getAll();
+    KtpEntity getById(Integer id);
+    KtpEntity update(Integer id, KtpDto dto);
+    void delete(Integer id);
 }
