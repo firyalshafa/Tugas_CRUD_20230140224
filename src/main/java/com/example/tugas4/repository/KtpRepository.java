b4dd390183621a57f570fe53602cc2dff5856b46
firyalshafa <161413463+firyalshafa@users.noreply.github.com>
2026-03-13 14:10:33 +0900

    add KtpRepository with custom method existsByNomorKtp

--- src/main/java/com/example/tugas4/repository/KtpRepository.java
+++ src/main/java/com/example/tugas4/repository/KtpRepository.java
@@ -1,7 +1,11 @@
 package com.example.tugas4.repository;
 
 
+import com.example.tugas4.entity.KtpEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 @Repository
-public interface KtpRepository
-{}
+public interface KtpRepository extends JpaRepository<KtpEntity, Integer> {
+    boolean existsByNomorKtp(String nomorKtp);
+}
